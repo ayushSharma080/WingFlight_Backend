b9f7d7e02b9aa713d3c71dd5c3862dcb1386f727
ayushSharma080 <asharma12_be22@thapar.edu>
2026-02-03 15:23:13 +0530

    Add files via upload

---
+++ eurekaserver/src/main/java/com/example/demo/EurekaserverDemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaserverDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverDemo2Application.class, args);
+	}
+
+}
